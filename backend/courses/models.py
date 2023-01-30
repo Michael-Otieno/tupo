f24f2b29f7717e5cf510f3e818c38a74e5b9d136
michael-otieno <otieno.oduor.michael@gmail.com>
2023-01-30 11:12:21 +0300

    add course model

--- backend/courses/models.py
+++ backend/courses/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+  CATEGORIES = (
+    ('life skills','Life skills'),
+    ('children with disabilities','Children with disabilities'),
+    ('creative arts','Creative arts'),
+    ('sport programs','Sport programs'),
+  )
+  photo = models.ImageField(upload_to='img',default="",blank=True)
+  title = models.CharField(max_length=255)
+  description = models.CharField(max_length=500)
+  content = models.TextField()
+  class Meta:
+    ordering = ['id']
+
+  def __str__(self):
+    return self.title
+
+
+
+
+
