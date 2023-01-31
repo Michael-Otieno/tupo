7714740437b36235449466d6961f58725f66dfb9
michael-otieno <otieno.oduor.michael@gmail.com>
2023-01-31 13:20:11 +0300

    add register url

--- backend/courses/urls.py
+++ backend/courses/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path,include
 from . import views
-from .views import CourseView,CourseDetailView
+from .views import CourseView,CourseDetailView,RegisterView
 
 urlpatterns = [
     path("",views.getRoutes),
     path("courses/",CourseView.as_view()),
-    path("course/<int:pk>/",CourseDetailView.as_view())
+    path("course/<int:pk>/",CourseDetailView.as_view()),
+    path("register/",RegisterView.as_view())
 
 ]
