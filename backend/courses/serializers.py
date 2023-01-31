acc38c51654ccdb9b5e4f93ebaf6e4cf26bd9eaa
michael-otieno <otieno.oduor.michael@gmail.com>
2023-01-31 13:17:32 +0300

    add user serializer

--- backend/courses/serializers.py
+++ backend/courses/serializers.py
@@ -1,5 +1,27 @@
-from .models import Course
+from .models import Course,User
 from rest_framework import serializers
+
+
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ['id','first_name','last_name','id_number','phone_number','email','password']
+
+    extra_kwargs = {
+      'password':{'write_only':True}
+    }
+
+  def create(self, validated_data):
+    password = validated_data.pop('password', None)
+    instance = self.Meta.model(**validated_data)
+    if password is not None:
+        instance.set_password(password)
+    instance.save()
+    return instance
+
 
 class CourseSerializer(serializers.ModelSerializer):
   class Meta:
