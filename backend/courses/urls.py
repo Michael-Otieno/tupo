77fadf55dd3a9510bf5088d433962162bac5f769
michael-otieno <otieno.oduor.michael@gmail.com>
2023-02-01 07:55:07 +0300

    add MyTokenObtainPairView url

--- backend/courses/urls.py
+++ backend/courses/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path,include
 from . import views
-from .views import CourseView,CourseDetailView,RegisterView,LoginView
+from .views import CourseView,CourseDetailView,RegisterView,LoginView,MyTokenObtainPairView
+from rest_framework_simplejwt.views import (
+    # TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("",views.getRoutes),
@@ -8,5 +12,8 @@ urlpatterns = [
     path("course/<int:pk>/",CourseDetailView.as_view()),
     path("register/",RegisterView.as_view()),
     path('login/', LoginView.as_view()),
+
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
