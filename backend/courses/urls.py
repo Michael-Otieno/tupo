74cd1ac04bd623b63a87009657934b2e433eed15
michael-otieno <otieno.oduor.michael@gmail.com>
2023-02-01 07:24:05 +0300

    add login url

--- backend/courses/urls.py
+++ backend/courses/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path,include
 from . import views
-from .views import CourseView,CourseDetailView,RegisterView
+from .views import CourseView,CourseDetailView,RegisterView,LoginView
 
 urlpatterns = [
     path("",views.getRoutes),
     path("courses/",CourseView.as_view()),
     path("course/<int:pk>/",CourseDetailView.as_view()),
-    path("register/",RegisterView.as_view())
+    path("register/",RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
 
 ]
