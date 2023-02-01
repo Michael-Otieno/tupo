b8e2fac436b899ced38b357c83e8b8cd626d47f9
michael-otieno <otieno.oduor.michael@gmail.com>
2023-02-01 08:13:41 +0300

    add confirm password field

--- backend/courses/serializers.py
+++ backend/courses/serializers.py
@@ -6,21 +6,24 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+  password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
   class Meta:
     model = User
-    fields = ['id','first_name','last_name','id_number','phone_number','email','password']
+    fields = ['id','first_name','last_name','id_number','phone_number','email','password','password2']
 
     extra_kwargs = {
       'password':{'write_only':True}
     }
+  
+  def validate(self, attrs):
+    password = attrs.get('password')
+    password2=attrs.pop('password2')
+    if password != password2:
+      raise serializers.ValidationError("Password and Confirm Password doesn't match")
+    return attrs
 
-  def create(self, validated_data):
-    password = validated_data.pop('password', None)
-    instance = self.Meta.model(**validated_data)
-    if password is not None:
-        instance.set_password(password)
-    instance.save()
-    return instance
+  def create(self, validate_data):
+    return User.objects.create_user(**validate_data)
 
 
 class UserLoginSerializer(serializers.ModelSerializer):
